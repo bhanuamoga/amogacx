a67e43bb0d2c3008248cf2f95bcd17fe76e892bd
bhanuamoga <bhanu.amoga@gmail.com>
2026-01-12 18:00:43 +0530

    added tables,charts1

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -17,7 +17,7 @@ export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `[FOOTER MESSAGE]`; // not applicable
 export const CLEAR_BUTTON_TEXT: string = `Clear Conversation`;
 // export const PAGE_TITLE: string = `[PAGE TITLE]`; // this is the tab title on the browser
-export const PAGE_TITLE: string = `Amoga chatbot`; // this is the tab title on the browser
+export const PAGE_TITLE: string = `kidly.market ai assitant`; // this is the tab title on the browser
 
 // export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
